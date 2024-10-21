101cc2583d226e607b5fdf2a21aa7458f0ece452
ひろ <koki.hiro0723@gmail.com>
2024-10-22 03:18:57 +0900

    ダミーのHTTPサーバーをセットアップ

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// .envファイルから環境変数を読み込みます
-require("dotenv").config();
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
 
-// discord.jsライブラリの中から必要な設定を呼び出し、変数に保存します
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+import http from "http";
 
-// 環境変数からトークン、アプリケーションID、ギルドIDを取得します
-const token = process.env.DISCORD_TOKEN;
+dotenv.config();
 
-// クライアントインスタンスと呼ばれるオブジェクトを作成します
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -16,18 +13,16 @@ const client = new Client({
   ],
 });
 
-// クライアントオブジェクトが準備OKとなったとき一度だけ実行されます
 client.once(Events.ClientReady, (c) => {
   console.log(`準備OKです! ${c.user.tag}がログインします。`);
 });
 
 client.on("messageCreate", (message) => {
-  if (message.author.bot) return; //BOTのメッセージには反応しない
+  if (message.author.bot) return;
 
   if (message.content === "こんにちわ") {
     message.channel.send("こんにちわ！");
   }
 });
 
-// ログインします
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
